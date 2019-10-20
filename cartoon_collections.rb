af0682d80c6fa89368e25beeb5ef2d044090c551
kelly r maass <kmaass77627@gmail.com>
2019-10-20 21:54:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,9 @@ def roll_call_dwarves(array)
   array.each do |name|
     puts name
 end
-roll_call_dwarves(array)
 end
 
+roll_call_dwarves(["Doc", "Dopey"])
 
 def summon_captain_planet# code an argument here
   # Your code here
