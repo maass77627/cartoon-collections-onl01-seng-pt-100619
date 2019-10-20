0c1b514b2fc5f9a3b6a5ecaa8aad4c6e1ba997a4
kelly r maass <kmaass77627@gmail.com>
2019-10-20 21:42:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,8 @@ def roll_call_dwarves(array)
   array.each_with_index do |name,index|
     new_array << #{index} #{name}
 end
+new_array
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
