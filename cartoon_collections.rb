537fc70471e165b92785563f593fb53419ebdf9e
kelly r maass <kmaass77627@gmail.com>
2019-10-20 22:36:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,11 @@ def summon_captain_planet(planeteer_calls)
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+ calls.each do |call|
 end
+return false 
+end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
